0fe6c39d32ab931d15b546b0a4b9cd3d2bd3d8ad
dejanle <dejnik978@gmail.com>
2017-07-09 18:32:14 +0200

    remove App.js, App.css, add Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="padding-top footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import {
   BrowserRouter as Router,
@@ -10,38 +9,40 @@ import {
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 ReactDOM.render(
-
+<div>
   <Router>
-    <div>
-     <nav className="navbar navbar-inverse navbar-fixed-top">
-          <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <Link  to="/" activeclassName="activeNav" className="navbar-brand">Portfolio</Link>
-            </div>
-            <div id="navbar" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-                  <li><Link exact activeclassName="activeNav"  to="/">Home</Link></li>
-                  <li><Link  to="/about"  activeclassName="activeNav">About</Link></li>
-                  <li><Link  to="/contact" activeclassName="activeNav">Contact</Link></li>
-            </ul>
-            </div>
-          </div>
-        </nav>
+      <div>
+         <nav className="navbar navbar-inverse navbar-fixed-top">
+              <div className="container">
+                <div className="navbar-header">
+                  <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                  <Link  to="/" activeclassName="activeNav" className="navbar-brand">Portfolio</Link>
+                </div>
+                <div id="navbar" className="collapse navbar-collapse">
+                <ul className="nav navbar-nav">
+                      <li><Link exact activeclassName="activeNav"  to="/">Home</Link></li>
+                      <li><Link  to="/about"  activeclassName="activeNav">About</Link></li>
+                      <li><Link  to="/contact" activeclassName="activeNav">Contact</Link></li>
+                </ul>
+                </div>
+              </div>
+            </nav> 
 
-
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/contact" component={Contact}/>
-      </div>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+    </div>
     </Router>
+    <Footer />
+      </div>
   ,
   document.getElementById('root')
 
